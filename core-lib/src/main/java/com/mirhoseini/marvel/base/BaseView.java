6a19fd00eb3e01b54731d37a19ccdab076195169
Mohsen Mirhoseini Argi <msn.mirhoseini@gmail.com>
2016-11-28 22:45:51 +0330

    Fix package name issue

---
+++ core-lib/src/main/java/com/mirhoseini/marvel/base/BaseView.java
@@ -0,0 +1,13 @@
+package com.mirhoseini.marvel.base;
+
+/**
+ * Created by Mohsen on 20/10/2016.
+ */
+
+public interface BaseView {
+
+    void showMessage(String message);
+
+    void showOfflineMessage();
+
+}
